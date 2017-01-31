d35024e850bcfd07541e7184caa40cd0287644e4
Ben Jay <fakebenjay@gmail.com>
2017-01-31 07:34:23 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,25 @@
-# Code your solution here!
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+
+  input = nil
+  winner = rand(1..6)
+
+  while input != "exit"
+    input = gets.chomp
+    guess = input.to_i
+    valid_guesses = (1..6).to_a
+
+    if guess != winner && valid_guesses.include?(guess)
+      1.times do puts "The computer guessed #{winner}."
+      end
+    elsif guess == winner
+      1.times do puts "You guessed the correct number!"
+      end
+    else
+      1.times do puts "Invalid entry, try again."
+      end
+    end
+    break if valid_guesses.include?(guess)
+  end
+  puts "Goodbye!"
+end
--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -11,14 +11,18 @@ describe "Guessing CLI" do
     it "responds to a correct guess" do
       allow(self).to receive(:rand).and_return(2)
       expect(self).to receive(:gets).and_return("2")
-      expect(self).to receive(:gets).and_return("exit")
+      #expect(self).to receive(:gets).and_return("exit")
+      ## Thought it'd be better to immediately kill program after valid guess
+      ## Earlier unsaved versions of this lab required an extra "exit"
       expect { run_guessing_game }.to output(/You guessed the correct number!/).to_stdout
     end
 
     it "responds to an incorrect guess" do
       allow(self).to receive(:rand).and_return(5)
       expect(self).to receive(:gets).and_return("2")
-      expect(self).to receive(:gets).and_return("exit")
+      #expect(self).to receive(:gets).and_return("exit")
+      ## Thought it'd be better to immediately kill program after valid guess
+      ## Earlier unsaved versions of this lab required an extra "exit"
       expect { run_guessing_game }.to output(/The computer guessed 5./).to_stdout
     end
   end
